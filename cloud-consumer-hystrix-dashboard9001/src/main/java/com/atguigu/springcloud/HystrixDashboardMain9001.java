1cc5f3f8e40379141e08becd9ec18cfc53243927
‘lidengqiang’ <dandelion66890@163.com>
2022-11-14 22:04:55 +0800

    901 hello

--- cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -8,12 +9,13 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  * @auther zzyy
  * @create 2019-11-12 14:31
  */
+@Slf4j
 @SpringBootApplication
 @EnableHystrixDashboard
 public class HystrixDashboardMain9001
 {
     public static void main(String[] args)
-    {
+    {    log.info("901 hello");
         SpringApplication.run(HystrixDashboardMain9001.class,args);
     }
 }
