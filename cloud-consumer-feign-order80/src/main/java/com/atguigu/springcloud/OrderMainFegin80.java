b3e2d185b6ccaf7a91edc1dbe1080a4f664fb0b9
‘lidengqiang’ <dandelion66890@163.com>
2022-11-14 22:07:46 +0800

    80 hello

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderMainFegin80.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderMainFegin80.java
@@ -1,13 +1,15 @@
 package com.atguigu.springcloud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+@Slf4j
 @EnableFeignClients
 @SpringBootApplication
 public class OrderMainFegin80 {
     public static void main(String[] args) {
+        log.info("80hello");
         SpringApplication.run(OrderMainFegin80.class);
     }
 
