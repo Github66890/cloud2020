e45cd217d9f5fda28f77cb55db47ade85cef8bb5
‘lidengqiang’ <dandelion66890@163.com>
2022-11-14 22:03:58 +0800

    order hello

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMainApp80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMainApp80.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud;
 
 import com.atguigu.myrule.MySelfRule;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,11 +11,14 @@ import org.springframework.cloud.netflix.ribbon.RibbonClients;
 /**
  * @author 86187
  */
+@Slf4j
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration= MySelfRule.class)
 @EnableEurekaClient
 @SpringBootApplication
 public class OrderMainApp80 {
+
     public static void main(String[] args) {
+        log.info("hello");
         SpringApplication.run(OrderMainApp80.class);
     }
 }
