35b2f45cd8e8d043fbaf4f0b789bbd5ee996dfe0
‘lidengqiang’ <dandelion66890@163.com>
2022-11-14 21:45:45 +0800

    this is 8006 demo intro

--- cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/PaymentMain8006.java
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/PaymentMain8006.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,12 +9,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @auther zzyy
  * @create 2020-01-31 12:00
  */
+@Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PaymentMain8006
 {
     public static void main(String[] args)
     {
+        log.info("this is payment main 8006!");
         SpringApplication.run(PaymentMain8006.class,args);
     }
 }
