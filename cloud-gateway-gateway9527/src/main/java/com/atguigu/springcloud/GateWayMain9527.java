2e6d5e6cc405a8c78a3d9c3c4ee1aa60c858b63e
‘lidengqiang’ <dandelion66890@163.com>
2022-11-14 21:37:39 +0800

    add log info

--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,12 +9,15 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @auther zzyy
  * @create 2020-02-06 10:17
  */
+@Slf4j
 @SpringBootApplication
 @EnableEurekaClient
 public class GateWayMain9527
 {
+
     public static void main(String[] args)
     {
+        log.info("hello");
         SpringApplication.run(GateWayMain9527.class,args);
     }
 }
